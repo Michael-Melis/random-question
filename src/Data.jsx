1aa1bc2c1ac626073ab5784aa7a4940011432c1c
Michael Melis <michael.melis169@gmail.com>
2022-02-27 13:27:54 +0100

    build: random question

---
+++ src/Data.jsx
@@ -0,0 +1,341 @@
+export const questions = [
+  {
+    id: 1,
+    question: "Tell me about a time you obsessed over giving very high-quality service to a customer",
+
+  },
+  {
+    id: 2,
+    question: "Can you describe a difficult interaction you’ve had with a customer?",
+
+  },
+  {
+    id: 3,
+    question: "Can you provide an example of when you asked a customer for feedback? How did   you use that feedback to drive innovation or improvement?",
+
+  },
+  {
+    id: 4,
+    question: "Have you ever refused to indulge an unreasonable request from a customer?",
+
+  },
+  {
+    id: 5,
+    question: "Give me an example of a time you used customer feedback to drive improvement or  innovation. What was the situation, and what action did you take?",
+
+  },
+  {
+    id: 6,
+    question: "Tell me about a time when you went above and beyond the call of duty for a customer. Why did you take the action you did? What was the outcome?",
+
+  },
+  {
+    id: 7,
+    question: "Tell me about a time when you gave a simple solution to a complex problem.",
+
+  },
+  {
+    id: 8,
+    question: "Tell me about a time when you invented something.",
+
+  },
+  {
+    id: 9,
+    question: "Please walk me through a different scenario of a process you invented or improved.",
+
+  },
+  {
+    id: 10,
+    question: "What’s the most innovative new idea that you have implemented?",
+
+  },
+  {
+    id: 11,
+    question: "Tell me about a time you were able to make something significantly simpler forcustomers. What drove you to implement this change?",
+
+  },
+  {
+    id: 12,
+    question: "Example of a time when you proposed a novel approach to a problem.",
+
+  },
+  {
+    id: 13,
+    question: "Give me an example of when you used data to make a decision/solve a problem.",
+
+  },
+  {
+    id: 14,
+    question: "Tell me about the most complex problem you’ve ever worked on. - redundancies,not having an employee feedback program",
+
+  },
+  {
+    id: 15,
+    question: "Tell me about a problem you had to solve that required in-depth thought and analysis. How did you know you were focusing on the right things?",
+
+  },
+  {
+    id: 16,
+    question: "Tell me about a time you were trying to understand a problem on your team, and you had to go down several layers to figure it out. Who did you talk with, and what info proved most valuable? How did you use that info to help solve the problem?",
+
+  },
+  {
+    id: 17,
+    question: "Tell me about a time when you had to work with limited time or resources (D1C:technically, the “limited time” element fits more under Deliver Results, which is why I don’t believe that this would have been the precise question that interviewers asked",
+
+  },
+  {
+    id: 18,
+    question: "Describe a time when you had to manage a budget (or manage time/money/resources/etc.). Were you able to get more out of less?",
+
+  },
+  {
+    id: 19,
+    question: "Tell me about a time where you thought of a new way to save money for the company.",
+
+  },
+  {
+    id: 20,
+    question: "Give me two examples of when you did more than what was required in any jobexperience (D1C: I don’t know why the interviewer asked for two situations)",
+
+  },
+  {
+    id: 21,
+    question: "Tell me about a time when you took on something significant outside your area of responsibility, and why was that important?",
+
+  },
+  {
+    id: 22,
+    question: "Describe a time you didn’t think you were going to meet a commitment you promised?",
+
+  },
+  {
+    id: 23,
+    question: "Tell me about a critical piece of feedback you received",
+
+  },
+  {
+    id: 24,
+    question: "Tell me about a time when you saw your team member was struggling.",
+
+  },
+  {
+    id: 25,
+    question: "Building trust with teams can be difficult to achieve sometimes. Can you give me an example of how you effectively built trusting working relationships with others?",
+
+  },
+  {
+    id: 26,
+    question: "Can you describe a time you needed the cooperation of a resistant peer? What was the situation, and how did you handle that?",
+
+  },
+  {
+    id: 27,
+    question: "Your team goals were misaligned with another team. How were you able to match this up?",
+  },
+  {
+    id: 28,
+    question: "Tell me about when you took an unpopular stance in a meeting with peers and your leader, and you were the outlier.",
+
+  },
+  {
+    id: 29,
+    question: "Tell me about a time when you decide to go along with the group decision, even if you disagree.",
+
+  },
+  {
+    id: 30,
+    question: "Tell me about a time when you strongly disagreed with your manager on something you deemed very important to the business. What was it about, and how did you  handle it?",
+
+  },
+  {
+    id: 31,
+    question: "Tell me about a time when you dealt with an employee with poor performance.",
+
+  },
+  {
+    id: 32,
+    question: "Tell me about a time when you coached someone into outstanding performance.",
+
+  },
+  {
+    id: 33,
+    question: "Tell me about a time when you’ve built a team for a specific project.",
+
+  },
+  {
+    id: 34,
+    question: "Can you give me an example of a calculated risk you have taken where speed was critical? What was the situation, and how did you handle it?",
+
+  },
+  {
+    id: 35,
+    question: "Describe a situation where you made an important business decision without consulting your manager",
+
+  },
+  {
+    id: 36,
+    question: "Tell me about when you had to analyse facts quickly, define key issues, and respond immediately to a situation. What was the outcome?",
+
+  },
+  {
+    id: 37,
+    question: "Tell me about a time when you were leading a group, were assigned a goal, and did  not reach it",
+
+  },
+  {
+    id: 38,
+    question: "Tell me about a time where you not only met a goal but considerably exceeded expectations.",
+
+  },
+  {
+    id: 39,
+    question: "Tell me about a time when you were able to deliver an important project under a tight deadline.",
+
+  },
+  {
+    id: 40,
+    question: "Tell me about a time when you did not effectively manage your project and did not complete something on time?",
+
+  },
+  {
+    id: 41,
+    question: "Tell me about a time when you did not effectively manage your project and did not  complete something on time?",
+
+  },
+  {
+    id: 42,
+    question: "Tell me about a time when you could remove a serious roadblock/barrier preventing your team from making progress? How were you able to remove the barrier? Whatwas the outcome?",
+
+  },
+  {
+    id: 43,
+    question: "Tell me about a time when you were more than halfway and realized it was the wrong goal. - nie wrong goal ale klient zmenil nazor",
+
+  },
+  {
+    id: 44,
+    question: "Tell me about a time when you have been unsatisfied with the status quo. What did you do to change it? Were you successful?",
+
+  },
+  {
+    id: 45,
+    question: "Tell me about a time when you were dissatisfied with the quality of something at work and went out of your way to improve it",
+
+  },
+  {
+    id: 46,
+    question: "When you refused to compromise around quality or customer service",
+
+  },
+  {
+    id: 47,
+    question: "What’s the coolest thing you’ve learned on your own that you’ve then been able to apply in your job and perform your job further?",
+
+  },
+  {
+    id: 48,
+    question: "Tell me about a time where someone openly challenged you. How did you handle this feedback? (D1C: The way this question is phrased makes it sound like Have  Backbone; Disagree and Commit. In reality, this is probably a Learn & Be Curious  question)",
+
+  },
+  {
+    id: 49,
+    question: "Give an example of a tough or critical piece of feedback you received. What was it, and what did you do about",
+
+  },
+  {
+    id: 50,
+    question: "Tell me about a time you were working on an initiative or goal and saw an opportunity to do something much bigger than the initial focus.",
+
+  },
+  {
+    id: 51,
+    question: "Tell me about a time when very senior people adopted your vision across the  organisation.",
+
+  },
+  {
+    id: 52,
+    question: "Tell me about a time when you had to work with incomplete data or information (or can you think of a time when you had to make a decision but you didn’t have enough data to be sure you were making the right decision? What did you do and how did it work out?)",
+
+  },
+  {
+    id: 53,
+    question: "Can you think of a time you made a bad professional decision? What was the impact  of the decision? What did you learn?",
+
+  },
+  {
+    id: 54,
+    question: "When you had to make a difficult decision with inputs from a lot of people ",
+
+  },
+  {
+    id: 55,
+    question: "Tell me about a time when someone on your team challenged you to think differently about a problem. What was the situation? How did you respond? What was the outcome? old building, but problem was also customer service from some employees",
+
+  },
+  {
+    id: 56,
+    question: "Name a project you were involved with? redundancies ",
+
+  },
+  {
+    id: 57,
+    question: "What do you aspire for in your career? ",
+
+  },
+  {
+    id: 58,
+    question: "Tell me about a time you couldn't deliver on a commitment. What were the obstacles? did not complete redundancies on deadline ",
+
+  },
+  {
+    id: 59,
+    question: "Tell me about a time you created a metric ",
+
+  },
+  {
+    id: 60,
+    question: "How would you deliver an unpopular message to your co-workers. - in person or by call, benefits ",
+
+  },
+  {
+    id: 61,
+    question: "How would you determine the price for a new product being launched in a new market? ",
+
+  },
+  {
+    id: 62,
+    question: "Describe a situation where you had to earn a customer's trust. ",
+
+  },
+  {
+    id: 63,
+    question: "Tell me about a time you didn't succeed and how did it make you feel? interview ",
+
+  },
+  {
+    id: 64,
+    question: "Tell me about a time when you had to support an unpopular decision - overtime",
+
+  },
+  {
+    id: 65,
+    question: "Can you give an example when you did something for a customer that would reflect badly on you or your department?",
+
+  },
+  {
+    id: 66,
+    question: "Give an example of a time when you received feedback that you didn't agree with? ",
+
+  },
+  {
+    id: 67,
+    question: "Can you give me a situation in which the answer to a problem did not appear immediately but required you to do in-depth analysis ? ",
+
+  },
+  {
+    id: 68,
+    question: "Tell me about a time you felt you let someone down at work. What happened, what did you do to try and rectify the situation, and what did you learn from the experience? ",
+
+  },
+]
